e7a5c3836908db3ccf390193d89e8bb07e099593
Dave Dehghani <davedehghani@gmail.com>
2017-01-12 17:33:27 +0000

    Print the index of the waldo

--- w1d4-remote/waldo2.js
+++ w1d4-remote/waldo2.js
@@ -1,13 +1,14 @@
- var search = findWaldo(arr, found) {
-  search.forEach(function() {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback})
+
+function FindWaldo(arr, found) {
+  arr.forEach(function(element, index) {
+    if (element === "Waldo") {
+      found(index);   // execute callback})
     }
-  }
+  })
 }
 
 function actionWhenFound(index) {
  console.log("Found Waldo at index " + index);
 }
 
- findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+FindWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
